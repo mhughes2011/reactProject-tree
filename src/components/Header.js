776fdbcd5f1516273232098e2daf0991512b5488
M Hughes <mhughes2011@gmail.com>
2021-07-20 14:43:00 -0700

    implemented props destructuring

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,15 @@ import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
 const Header = (props) => {
-
-    return (
-      <header>
-        <Stats players={props.players}/>
-        <h1>{ props.title }</h1>
-        <Stopwatch />
-      </header>
-    );
+  const {players, title} = props;
+  //This is destructuring props.  You could also do this by bringing in the destructured object in the function declaration like this const Header = ({players, title}) => ...
+  return (
+    <header>
+      <Stats players={players}/>
+      <h1>{title}</h1>
+      <Stopwatch />
+    </header>
+  );
 }
 
 export default Header;
