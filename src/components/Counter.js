97bafea65c3aa39dc0e3e060b44cd223062eac05
M Hughes <mhughes2011@gmail.com>
2021-07-21 16:07:02 -0700

    added validation through PropTypes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Counter = ({index, score, changeScore}) => {
   return (
@@ -10,5 +11,11 @@ const Counter = ({index, score, changeScore}) => {
   );
 
 }
+
+Counter.PropTypes = {
+  index: PropTypes.number,
+  score: PropTypes.number,
+  changeScore: PropTypes.func
+};
 
 export default Counter;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
@@ -14,5 +15,10 @@ const Header = (props) => {
     </header>
   );
 }
+
+Header.PropTypes = {
+  title: PropTypes.string,
+  players: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default Header;
